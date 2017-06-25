a8e8a999ae9f367b5ff0cfdcb78b2934152212f5
EQwon <bluetwintail@naver.com>
2017-06-25 14:59:17 +0900

    기초 이동 구현

---
+++ Assets/Scripts/MovingCursorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCursorController : MonoBehaviour
+{
+	private Vector2 origin;
+	private Vector2 movingVector;
+
+	private void Start()
+	{
+		origin = transform.position;
+		movingVector = Vector2.zero;
+	}
+
+	private void Update()
+	{
+		TellMovingVectorToPlayer(movingVector);
+	}
+
+	public void TrackingMouse()
+	{
+		movingVector = new Vector2 (Input.mousePosition.x, Input.mousePosition.y) - origin;
+		
+		transform.position = CursorPos(movingVector);
+	}
+
+	public void GoToOrigin()
+	{
+		transform.position = origin;
+		movingVector = Vector2.zero;
+	}
+
+	private Vector2 CursorPos(Vector2 movingVector)
+	{
+		if(movingVector.magnitude <= 100f)
+		{
+			return movingVector + origin;
+		}
+
+		return origin + movingVector.normalized * 100;
+	}
+
+	private void TellMovingVectorToPlayer(Vector2 movingVector)
+	{
+		PlayerController.movingVector = movingVector.normalized;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public static Vector2 movingVector;
+	public float moveSpeed;
+
+	private void Start()
+	{
+		movingVector = Vector2.zero;
+	}
+	
+	private void Update()
+	{
+		transform.position = new Vector2(transform.position.x + movingVector.x * Time.deltaTime * moveSpeed, transform.position.y + movingVector.y * Time.deltaTime * moveSpeed);
+	}
+}
