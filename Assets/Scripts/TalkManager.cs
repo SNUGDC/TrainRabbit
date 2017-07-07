163ed70c0790b2071ae8cf0d996a3fe503737253
EQwon <bluetwintail@naver.com>
2017-07-07 21:57:12 +0900

    원생 토끼랑 대화 시작하는 디버그 로그를 띄울 수 있다!!

---
+++ Assets/Scripts/TalkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+	public bool isMeetPostgraduateRabbit;
+	public bool isClickPostgraduateRabbit;
+
+	private void Start()
+	{
+		isMeetPostgraduateRabbit = false;
+		isClickPostgraduateRabbit = false;
+	}
+
+	private void Update()
+	{
+		if(isMeetPostgraduateRabbit == true && isClickPostgraduateRabbit == true)
+		{
+			Debug.Log("원생 토끼랑 대화 시작!");
+			isClickPostgraduateRabbit = false;
+		}
+	}
+}
---
+++ Assets/Scripts/TextStarter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextStarter : MonoBehaviour
+{
+	private TalkManager talkManager;
+
+	private void Start()
+	{
+		talkManager = GameObject.Find("Talk Manager").GetComponent<TalkManager>();
+	}
+
+	public void OnMouseDown()
+	{
+		if(transform.parent.gameObject.name.Contains("Postgraduate"))
+		{
+			talkManager.isClickPostgraduateRabbit = true;
+		}
+	}
+}
