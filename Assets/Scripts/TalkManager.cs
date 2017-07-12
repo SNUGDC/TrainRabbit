62101238e267a8231a325a33d72c67cdd5dbdb56
EQwon <bluetwintail@naver.com>
2017-07-12 14:33:02 +0900

    대화 프로토 일단 만듬

--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class TalkManager : MonoBehaviour
 {
+	public GameObject MovingPad;
+	public GameObject AttackPad;
+	public GameObject DialoguePanel;
+	public GameObject MainCamera;
+
 	public bool isMeetPostgraduateRabbit;
 	public bool isClickPostgraduateRabbit;
+
+	private GameObject Player;
 
 	private void Start()
 	{
 		isMeetPostgraduateRabbit = false;
 		isClickPostgraduateRabbit = false;
+		Player = GameObject.FindWithTag("Player").gameObject;
+		DialoguePanel.SetActive(false);
 	}
 
 	private void Update()
@@ -18,7 +27,31 @@ public class TalkManager : MonoBehaviour
 		if(isMeetPostgraduateRabbit == true && isClickPostgraduateRabbit == true)
 		{
 			Debug.Log("원생 토끼랑 대화 시작!");
+			DialogueStart();
 			isClickPostgraduateRabbit = false;
 		}
+	}
+
+	private void DialogueStart()
+	{
+		MovingPad.SetActive(false);
+		AttackPad.SetActive(false);
+		CloseUp();
+		DialoguePanel.SetActive(true);
+	}
+
+	private void CloseUp()
+	{
+		MainCamera.GetComponent<Camera>().orthographicSize = 5f;
+		MainCamera.transform.position = Player.transform.position + new Vector3(1, -1.5f, -10f);
+	}
+
+	public void DialogueEnd()
+	{
+		MainCamera.GetComponent<Camera>().orthographicSize = 8f;
+		MainCamera.transform.position = new Vector3(0, 0f, -10f);
+		MovingPad.SetActive(true);
+		AttackPad.SetActive(true);
+		DialoguePanel.SetActive(false);
 	}
 }
