3a9afc01358e293e3388732ba62c75bec77b32af
snugdc-computer <snugdc@gmail.com>
2017-07-20 18:15:05 +0900

    Train, TrainGenerator 스크립트 생성

--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class Train : MonoBehaviour
 {
 	public int trainNumber;
+
+    public bool IsThereNextTrain(bool isRightDoor)
+    {
+        if (trainNumber == 10 && isRightDoor == false)
+            return false;
+        else if (trainNumber == 1 && isRightDoor == true)
+            return false;
+
+        return true;
+    }
 }
---
+++ Assets/Scripts/TrainGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainGenerator : MonoBehaviour
+{
+    //하는 일 : 주인공 토끼가 누구고 몇번째 칸인지에 따라 열차 안의 토끼들을 생성
+    //지하철을 청소하는 함수와 생성하는 함수가 필요
+
+    public GameObject[] NormalRabbitPrefab;
+    public GameObject[] BadRabbitPrefab;
+    public GameObject[] GoodRabbitPrefab;
+
+    private Dictionary<string, GameObject> NormalRabbit;
+    private Dictionary<string, GameObject> BadRabbit;
+    private Dictionary<string, GameObject> GoodRabbit;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CreateRabbits();//지하철 내의 토끼들 생성
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DeleteAllRabbits();//지하철 내의 토끼들 제거
+        }
+    }
+
+    private void CreateRabbits(/*주인동 토끼의 레벨, 열차 칸 번호를 받아와야 함*/)
+    {
+
+    }
+
+    private void DeleteAllRabbits() //모든 토끼 제거
+    {
+    }
+}
