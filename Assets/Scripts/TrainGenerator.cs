959eee15e93cc56b7b2873c6b2a3a598e08ec9af
snugdc-computer <snugdc@gmail.com>
2017-07-20 20:59:00 +0900

    토끼 생성하는 스크립트 짜는 중

--- Assets/Scripts/TrainGenerator.cs
+++ Assets/Scripts/TrainGenerator.cs
@@ -7,9 +7,9 @@ public class TrainGenerator : MonoBehaviour
     //하는 일 : 주인공 토끼가 누구고 몇번째 칸인지에 따라 열차 안의 토끼들을 생성
     //지하철을 청소하는 함수와 생성하는 함수가 필요
 
-    public GameObject[] NormalRabbitPrefab;
-    public GameObject[] BadRabbitPrefab;
-    public GameObject[] GoodRabbitPrefab;
+    public RabbitDictionary[] NRD;
+    public RabbitDictionary[] BRD;
+    public RabbitDictionary[] GRD;
 
     private Dictionary<string, GameObject> NRdic;
     private Dictionary<string, GameObject> BRdic;
@@ -17,7 +17,7 @@ public class TrainGenerator : MonoBehaviour
 
     private void Start()
     {
-        BRdic.Add("잡상인", BadRabbitPrefab[0]); //암튼 이런식으로 해야함
+        ArrayToDictionary();
     }
 
     private void Update()
@@ -35,10 +35,32 @@ public class TrainGenerator : MonoBehaviour
 
     private void CreateRabbits(/*주인동 토끼의 레벨, 열차 칸 번호를 받아와야 함*/)
     {
-
+        Instantiate(GRdic["OldGentleman"]);
+        Instantiate(GRdic["Postgraduate"]);
+        Instantiate(GRdic["College"]);
     }
 
     private void DeleteAllRabbits() //모든 토끼 제거
     {
+    }
+
+    private void ArrayToDictionary()
+    {
+        NRdic = new Dictionary<string, GameObject>();
+        BRdic = new Dictionary<string, GameObject>();
+        GRdic = new Dictionary<string, GameObject>();
+
+        foreach (RabbitDictionary rabbitDic in NRD)
+        {
+            NRdic.Add(rabbitDic.RabbitName, rabbitDic.RabbitPrefab);
+        }
+        foreach (RabbitDictionary rabbitDic in BRD)
+        {
+            BRdic.Add(rabbitDic.RabbitName, rabbitDic.RabbitPrefab);
+        }
+        foreach (RabbitDictionary rabbitDic in GRD)
+        {
+            GRdic.Add(rabbitDic.RabbitName, rabbitDic.RabbitPrefab);
+        }
     }
 }
