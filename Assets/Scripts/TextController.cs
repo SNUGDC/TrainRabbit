31ebd3d894d8713a73f635ad1df240c1b3ee68e0
DESKTOP-GM4CT7E\quo10 <quo1031@gmail.com>
2017-07-27 11:53:53 +0900

    csv parser

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -5,21 +5,37 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour {
 
+    public TextAsset ttext;
+
+
     Text nextName;
     Text nextText;
 
     // Use this for initialization
     void Start()
     {
+        Debug.Log(ttext.text);
+        DialougeReader reader = new DialougeReader();
+        reader.Load(ttext);
+        string[] strArray = { };
+
+        foreach (var row in reader.GetRowList())
+        {
+
+            Debug.Log(row.name + ": " + row.dialogue);
+        }
         nextName = GameObject.Find("Name").GetComponent<Text>();
         nextText = GameObject.Find("Text").GetComponent<Text>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) {
         nextName.text = "LOL";
         nextText.text = "WTFF";
+        }
     }
 
 
