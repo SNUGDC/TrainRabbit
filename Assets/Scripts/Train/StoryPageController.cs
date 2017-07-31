078da6ead537060021bc898e72393acc4b26ce0f
EQwon <bluetwintail@naver.com>
2017-08-01 07:03:57 +0900

    오프닝화면 추가
    충

---
+++ Assets/Scripts/Train/StoryPageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryPageController : MonoBehaviour {
+
+    public GameObject StoryPageControl;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        
+        StoryPageControl.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            StoryPageControl.SetActive(false);
+        }
+	}
+}
