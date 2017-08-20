49d44e6abffb678140546a689741fc63d502deef
EQwon <bluetwintail@naver.com>
2017-08-21 02:50:20 +0900

    아이템 박스 스크립트 생성 및 이상한 파일들 삭제

---
+++ Assets/Scripts/ItemBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemBox : MonoBehaviour
+{
+	public string ItemName;
+	public Sprite ItemImage;
+
+	private void Start()
+	{
+		if(PlayerPrefs.HasKey(ItemName))
+		{
+			GetComponent<Image>().sprite = ItemImage;
+			GetComponent<Image>().SetNativeSize();
+		}
+	}
+}
