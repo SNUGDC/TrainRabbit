563e4da12dae878f664407a1d6d2e76b1e8e830b
EQwon <bluetwintail@naver.com>
2017-08-21 03:07:33 +0900

    아이템 박스 스크립트 수정

--- Assets/Scripts/ItemBox.cs
+++ Assets/Scripts/ItemBox.cs
@@ -7,6 +7,7 @@ public class ItemBox : MonoBehaviour
 {
 	public string ItemName;
 	public Sprite ItemImage;
+	public int Amount;
 
 	private void Start()
 	{
@@ -14,6 +15,8 @@ public class ItemBox : MonoBehaviour
 		{
 			GetComponent<Image>().sprite = ItemImage;
 			GetComponent<Image>().SetNativeSize();
+
+			Amount = PlayerPrefs.GetInt(ItemName);
 		}
 	}
 }
