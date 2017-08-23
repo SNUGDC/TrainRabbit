ec84782000ce6c753eab9f9ea7c5159b43fecb7f
JeJunChoi <alphacanis35@gmail.com>
2017-08-23 20:10:02 +0900

    인성계승, 뉴게임, 저장필요없음, 대학스테이지오프닝

---
+++ Assets/Scripts/Train/Timestop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timestop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void Timepause()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void Timeresume()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
