b0ae90caefc430d33a452ef843b67c05b0cbb250
choongchoongee <quo1031@naver.com>
2017-08-24 21:43:39 +0900

    소리 안됨

---
+++ Assets/MusicSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour
+{
+
+    public AudioClip newTrack;
+
+    private MusicManager theMM;
+
+    // Use this for initialization
+    void Start()
+    {
+        theMM = FindObjectOfType<MusicManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
