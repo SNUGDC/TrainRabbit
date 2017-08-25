5ad209a75dc5fbd5fc54d7834bccb4afa3e7f157
EQwon <bluetwintail@naver.com>
2017-08-25 20:47:23 +0900

    게임 리셋 구현

---
+++ Assets/Scripts/GameReset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameReset : MonoBehaviour
+{
+	public void Reset()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+}
