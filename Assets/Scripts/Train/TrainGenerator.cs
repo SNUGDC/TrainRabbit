53779d0784bc19cd3cf3a008c8ec7d947f0fca6b
EQwon <bluetwintail@naver.com>
2017-08-25 22:01:53 +0900

    트레인 제너레이터 복구

--- Assets/Scripts/Train/TrainGenerator.cs
+++ Assets/Scripts/Train/TrainGenerator.cs
@@ -26,6 +26,8 @@ public class TrainGenerator : MonoBehaviour
     private List<GameObject> Rabbits;
     private GameObject NowTrain;
     private float[] chairPosX = new float[12] {-12.8f, -11.3f, -9.8f, -4.4f, -2.8f, -1.2f, 0.4f, 2.0f, 3.6f, 9.1f, 10.7f, 12.2f};
+    private float gongikInstanceCooltime;
+    public float GongikInstanceCooltime { get { return (Mathf.Clamp(gongikInstanceCooltime, 0, 10)); } set { gongikInstanceCooltime = value; } }
 
     private void Start()
     {
@@ -38,10 +40,6 @@ public class TrainGenerator : MonoBehaviour
 
         Instantiate(musicManager);
 
-        if(PlayerData.Conscience < 20)
-        {
-            Rabbits.Add(Instantiate(BRdic["Gongik"]));
-        }
     }
 
     private void Update()
@@ -49,6 +47,17 @@ public class TrainGenerator : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             DeleteAllRabbits();//지하철 내의 모든 토끼들 제거
+        }
+                GongikInstanceCooltime += Time.deltaTime;
+        if(PlayerData.Conscience < 20 && GongikInstanceCooltime > 6f)
+        {
+            Debug.Log("lowCs");
+            if(!FindObjectsOfType<BadRabbitController>().Any(a => a.badRabbit == BadRabbit.Gongik))
+            {
+                GameObject newGongik = Instantiate(BRdic["Gongik"]);
+                newGongik.GetComponent<BadRabbitController>().tr = this;
+                Rabbits.Add(newGongik);
+            }
         }
     }
 
@@ -505,10 +514,14 @@ public class TrainGenerator : MonoBehaviour
 
     private void CreateItem()
     {
-        int randomNum = Random.Range(0, ItemDictionary.Length);
-        Vector2 spawnPos = new Vector2(Random.Range(-12.5f, 12.5f), Random.Range(-5f, 3f));
+        if (ItemDictionary.Length != 0)
+        {
+            int randomNum = Random.Range(0, ItemDictionary.Length);
+            Vector2 spawnPos = new Vector2(Random.Range(-12.5f, 12.5f), Random.Range(-5f, 3f));
 
-        Instantiate(ItemDictionary[randomNum].ItemPrefab, spawnPos, Quaternion.identity);
+              Instantiate(ItemDictionary[randomNum].ItemPrefab, spawnPos, Quaternion.identity);
+        }
+        else return;
     }
 
     private void DeleteAllRabbits() //모든 토끼 제거
